238fdcd8372477fc2092789511a1c9718f48ba10
jiikko <n905i.1214@gmail.com>
2015-10-08 20:37:46 +0900

    f

--- lib/omakase_unicorn/rails.rb
+++ lib/omakase_unicorn/rails.rb
@@ -4,7 +4,7 @@ require 'rails/generators'
 module OmakaseUnicorn
   module Rails
     class ConfigGenerator < ::Rails::Generators::NamedBase
-      source_root File.expand_path('../templates', __FILE__)
+      source_root File.expand_path('../../templates', __FILE__)
 
       def init
         directory "templates", ".project"
